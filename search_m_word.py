6ec64cfea83325b4ab5515af2f093572044d108c
junhyeog <junhyeog@.korea.ac.kr>
2024-12-23 18:58:05 +0900

    feat: search_m_word

--- search_m_word.py
+++ search_m_word.py
@@ -1,23 +1,16 @@
 """
 임베딩 단위를 문장 + 단어로 구성
-
-problems:
-- 단어의 수가 많아서 임베딩이 오래 걸림
-- element의 inner text만을 하이라이트하지 못함
-
 """
-import re
 import streamlit as st
 from openai import OpenAI
 import openai
 import numpy as np
 from scipy.spatial.distance import cosine
-from apikey import apikey
+from apikey1 import apikey
 from nltk.tokenize import sent_tokenize, word_tokenize
 import colorsys
 import uuid
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain_openai.embeddings import OpenAIEmbeddings
+
 openai.api_key = apikey
 
 client = OpenAI(api_key=apikey)
@@ -29,12 +22,8 @@ def get_embedding(text, model="text-embedding-3-small"):
     return np.array(embedding)
 
 
-def calculate_cosine_similarity(a, b, norm=True):
-    sim = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-    if norm:
-        # normalize to 0~1
-        sim = (sim + 1) / 2
-    return sim
+def calculate_cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
 def update_sentence_embeddings(document):
@@ -43,21 +32,10 @@ def update_sentence_embeddings(document):
     return sentences, sentence_embeddings
 
 def update_word_embeddings(document):
-    # word_tokenize
-    # words = word_tokenize(document)
-    
-    # semantic chunker
-    # text_splitter = SemanticChunker(OpenAIEmbeddings(model="text-embedding-3-small", api_key=apikey))
-    # words = text_splitter.split_text(document)
-    
-    # naive method (split by space, punctuation)
-    words = document.split()
-    words = [word.strip(",.!?") for word in words]
-    words = [word for word in words if word]
-    
-    words = list(set(words))
+    words = word_tokenize(document)
     word_embeddings = [get_embedding(word) for word in words]
     return words, word_embeddings
+
 
 # default_colors = ["#ffff00", "#ffc0cb", "#ffa500", "#00ff00", "#add8e6"]
 def display_queries():
@@ -120,11 +98,11 @@ st.title("Semantic Search")
 document = st.text_area("Text:", height=300, key="document")
 if "pre_document" not in st.session_state:
     st.session_state["pre_document"] = ""
-similarity_threshold = st.slider("Similarity threshold", 0.1, 0.9, 0.2, 0.1)
+similarity_threshold = st.slider("Similarity threshold", 0.0, 1.0, 0.2, 0.1)
 
 if "query_count" not in st.session_state:
     add_query()
-
+ 
 display_queries()
 st.button("Add Query", on_click=add_query)
 
@@ -132,13 +110,11 @@ st.button("Add Query", on_click=add_query)
 def update_document():
     # sentence
     sentences, sentence_embeddings = update_sentence_embeddings(document)
-    print(f"[+] sentences: {len(sentences)}")
     st.session_state["pre_document"] = document
     st.session_state["sentences"] = sentences
     st.session_state["sentence_embeddings"] = sentence_embeddings
     # word
     words, word_embeddings = update_word_embeddings(document)
-    print(f"[+] words: {len(words)}")
     st.session_state["words"] = words
     st.session_state["word_embeddings"] = word_embeddings
 
@@ -155,23 +131,12 @@ if document:
         query_key = f"query_{query_info["uuid"]}"
         color_key = f"color_{query_info["uuid"]}"
         query = st.session_state.get(query_key)
-        color = st.session_state.get(color_key)
         to_be_updated = {}
         if query:
-            # exact match using regex (case insensitive)
-            matched = re.findall(re.escape(query), document, re.IGNORECASE)
-            matched = list(set(matched))
-            print(f"[+] matched ({len(matched)}): {matched}")
-            for match in matched:
-                if to_be_updated.get(match) is None:
-                    to_be_updated[match] = []
-                to_be_updated[match].append((color, 1.0))
-                
-            # semantic match
             query_embedding = get_embedding(query)
-
+            
             # sentence
-            chunks = [i for i in st.session_state.get("sentences", [])]
+            chunks = st.session_state.get("sentences", [])
             chunk_embeddings = st.session_state.get("sentence_embeddings", [])
             # word
             chunks.extend(st.session_state.get("words", []))
@@ -179,24 +144,22 @@ if document:
             
             for j, (chunk, chunk_embedding) in enumerate(zip(chunks,chunk_embeddings)):
                 similarity = calculate_cosine_similarity(
-                    query_embedding, chunk_embedding, norm=True
+                    query_embedding, chunk_embedding
                 )
-                
                 if similarity > similarity_threshold:
-                    # [similarity_threshold, similarity] => [0, 1]
-                    similarity = (similarity - similarity_threshold) / (1 - similarity_threshold)
-                    similarity = max(0.0, min(1.0, similarity))
+                    
+                    similarity = (similarity - similarity_threshold) / (
+                        1 - similarity_threshold
+                    )
                     if to_be_updated.get(chunk) is None:
                         to_be_updated[chunk] = []
-                    to_be_updated[chunk].append((color, similarity))            
+                    color = st.session_state.get(color_key)
+                    to_be_updated[chunk].append((color, similarity))
 
-        # highlight
         for chunk, highlights in to_be_updated.items():
             color = blend_colors(highlights)
             highlighted_sentence = f'<span style="background-color: rgba({color[0]}, {color[1]}, {color[2]}, {color[3]});">{chunk}</span>'
             highlighted_document = highlighted_document.replace(chunk, highlighted_sentence)
-            
-        
     highlighted_document = highlighted_document.replace("\n", "<br>")
     st.markdown(highlighted_document, unsafe_allow_html=True)
 else:
